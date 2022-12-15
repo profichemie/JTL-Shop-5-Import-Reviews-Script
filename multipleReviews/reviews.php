f5a798d6a282b42ea66a0426894231993a80c916
profichemie <120579020+profichemie@users.noreply.github.com>
2022-12-15 16:04:13 +0100

    Create reviews.php

---
+++ multipleReviews/reviews.php
@@ -0,0 +1,28 @@
+<?php
+
+$cReviews = array(
+				array(
+					'name'	=> 'Max Mustermann',
+					'title'	=> 'Muster Titel',
+					'text'	=> 'Mustertext',
+					'stars'	=> 5,
+					'date'	=> 'YYY-MM-DD'
+				),
+				array(
+					'name'	=> 'Max Mustermann',
+					'title'	=> 'Muster Titel',
+					'text'	=> 'Mustertext',
+					'stars'	=> 5,
+					'date'	=> 'YYY-MM-DD'
+				),
+				array(
+					'name'	=> 'Max Mustermann',
+					'title'	=> 'Muster Titel',
+					'text'	=> 'Mustertext',
+					'stars'	=> 5,
+					'date'	=> 'YYY-MM-DD'
+				)				
+);
+
+
+?>
